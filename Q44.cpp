37c91eb3cee7f1ffd3448c42e418a184d9fdf8cf
SonuAlex <instructable01@gmail.com>
2023-05-30 18:48:19 +0530

    Day 44 Complete
    56 Days Left
    30-05-2023

---
+++ Q44.cpp
@@ -0,0 +1,10 @@
+// Q44
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+// SOURCE CODE
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x, n);
+    }
+};
