82e1c643620ee413bdd72dfedb07d5a46cc90757
SonuAlex <instructable01@gmail.com>
2023-06-13 15:18:32 +0530

    Day 58 Complete
    42 Days Left
    13-06-2023

---
+++ Q58.cpp
@@ -0,0 +1,21 @@
+// Q58
+// A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+// You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+// Return the number of indices where heights[i] != expected[i].
+
+// SOURCE CODE
+class Solution {
+public:
+    int heightChecker(vector<int>& h) {
+        int n = h.size(), c = 0;
+        vector<int> e;
+        e = h;
+        sort(e.begin(),e.end());
+
+        for(int i=0; i<n; i++){
+            c += (e[i] != h[i]);
+        }
+
+        return c;
+    }
+};
