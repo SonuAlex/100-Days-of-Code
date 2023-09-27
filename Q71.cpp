9dc9f9c3dbe18a7c9f4c4cc81e20522162ee6fcc
SonuAlex <instructable01@gmail.com>
2023-09-27 17:28:35 +0530

    Day 71 Complete
    29 Days Left
    26-06-2023

---
+++ Q71.cpp
@@ -0,0 +1,16 @@
+// Q71
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+// SOURCE CODE
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        int i = 0;
+        while(pow(3,i) < n){
+            i++;
+        }
+        if(pow(3,i) == n) return true;
+        return false;
+    }
+};
