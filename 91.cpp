826dc04f6dc18845c3a34a5713d5e8e72e35b8f9
SonuAlex <instructable01@gmail.com>
2024-03-28 19:36:44 +0530

    Day 91 Complete
    9 Days Left

---
+++ 91.cpp
@@ -0,0 +1,18 @@
+// Q91
+// Minimize the sum of product
+
+// SOURCE CODE
+class Solution{
+    public:
+    long long int minValue(int a[], int b[], int n)
+    {
+        // Your code goes here
+        sort(a, a+n);
+        sort(b, b+n, greater<int>());
+        long long int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += a[i]*b[i];
+        }
+        return sum;
+    }
+};
